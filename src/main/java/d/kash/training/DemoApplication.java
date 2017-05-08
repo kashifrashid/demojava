c2c9582145c14d5b90028773b6cb58b6b8fdf63b
kashif rashid <krashid@kashifs-MacBook-Pro.local>
2017-05-08 11:39:41 +0100

    add in new

--- src/main/java/d/kash/training/DemoApplication.java
+++ src/main/java/d/kash/training/DemoApplication.java
@@ -1,10 +1,23 @@
 package d.kash.training;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
+	@Bean
+	public CommandLineRunner cmdRunner(@Value("${greeter.location") String greeterLocation) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println("demo application run from kash" + greeterLocation);
+			}
+		};
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
